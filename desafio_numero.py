0dc8d4f3d05fa977b94d17e1f52e1137b16df1c5
Matheus-1895 <matheus04.dev@gmail.com>
2024-07-05 15:56:15 -0300

    Codigos Python-DIO

--- desafio_numero.py
+++ desafio_numero.py
@@ -9,7 +9,7 @@ import re
 def validate_numero_telefone(phone_number):
    
     # TODO: Defina um padrão de expressão regular (regex) para validar números de telefone no formato (XX) 9XXXX-XXXX:
-    pattern = r'^\(\d{2}\) 9\d{4}-\d{4}$'
+    pattern = r"^\(\d{2}\)9\d{4}-\d{4}$"
    
     # A função 're.match()' para verifica se o padrão definido corresponde ao número de telefone fornecido.
     # O 're.match()' retorna um objeto 'match' se houver correspondência no início da string, caso contrário, retorna 'None'.
