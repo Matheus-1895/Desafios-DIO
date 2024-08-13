6717cea297759b1ae9dd59ec79538f98615b51a5
Matheus-1895 <matheus04.dev@gmail.com>
2024-08-12 21:44:16 -0300

    Codigos Python-DIO

--- while.py
+++ while.py
@@ -1,3 +1,25 @@
+nomes = []
+i = 1
+
+while i <= 5 :
+    nome = str(input("Digite um nome:"))
+    nomes.append(nome)
+    i += 1
+
+print(nomes)
+
+/////////
+
+n = int(input("Digite um numero"))
+
+numero = 0
+
+while numero <= n:
+    print(numero)
+    numero += 1
+
+//////
+
 opcao = -1
 
 while opcao != 0 and opcao < 3:
